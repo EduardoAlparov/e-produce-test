73cfbbf0cbf1b845116fba2ccc1e4f27be9381e2
EduardoAlparov <workman133@yandex.ru>
2023-11-14 10:38:49 +0500

    init commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import Modal from "./modules/Modal.js";
-import Map from "./modules/Map.js";
-import validationForm from './modules/validation.js';
-import inputMasks from './modules/masks.js';
-import lazyload from './modules/lazyload.js';
+// import Map from "./modules/Map.js";
+// import validationForm from './modules/validation.js';
+// import inputMasks from './modules/masks.js';
+// import lazyload from './modules/lazyload.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     const body = document.body;
@@ -22,29 +22,29 @@ document.addEventListener("DOMContentLoaded", () => {
         },
     });
 
-    // inits map: 
-    if(!document.getElementById('yamapsId')) {
-        return;
-    } else {
-        const map = new Map('yamapsId', {
-            // map center:
-            center: [55.77, 49.15],
-            // map zoom value:
-            zoom: 14,
-            // placemarks:
-            coords: [
-                [55.767398, 49.150961]
-            ],
-            // map control elements:
-            controls: [
-    
-            ]
-        })
-    }
+    // // inits map:
+    // if(!document.getElementById('yamapsId')) {
+    //     return;
+    // } else {
+    //     const map = new Map('yamapsId', {
+    //         // map center:
+    //         center: [55.77, 49.15],
+    //         // map zoom value:
+    //         zoom: 14,
+    //         // placemarks:
+    //         coords: [
+    //             [55.767398, 49.150961]
+    //         ],
+    //         // map control elements:
+    //         controls: [
 
-    validationForm();
-    inputMasks();
+    //         ]
+    //     })
+    // }
+
+    // validationForm();
+    // inputMasks();
 
     // lazyload for images, videos, iframes and objects:
-    lazyload();
+    // lazyload();
 })
